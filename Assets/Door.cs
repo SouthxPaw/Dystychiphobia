e077c260a18ed7973611ea7e0058e2828d0d24d5
SouthxPaw <40015860+SouthxPaw@users.noreply.github.com>
2019-10-17 13:03:31 -0400

    Game Files
    Everything needed to run the game in Unity

---
+++ Assets/Door.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    //variable to hold the index for the random level
+    private int randomLevel;
+    //array to hold the index of the level used
+    private ArrayList levelUsed = new ArrayList();
+    //variable to iterate through array
+    private int i = 0;
+    //variable to hold the max number of levels
+    private int maxLevel = 1;
+    //triggers event of player being in door collider box
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        //nothing needs to happen whenever player collides with it
+        if (collision.CompareTag("Player"))
+        {
+           //nothing needs to happen whenever player collides with it
+        }
+    }
+
+    //triggers event to load next level whenever player presses the interact key
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (Input.GetButtonDown("Interact"))
+            {
+                //generate a random level
+                randomLevel = Random.Range(2, maxLevel + 1);
+                //check to see if the level has been used and if it has keep randomizing the level until you find an unused level
+                while (levelUsed.Contains(randomLevel))
+                {
+                    if (levelUsed.Count == maxLevel)
+                    {
+                        break;
+                    }
+                    randomLevel = Random.Range(2, maxLevel + 1);
+                }
+                //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                SceneManager.LoadScene(randomLevel);
+                //store the random level into an array so that we can keep track of the level played
+                levelUsed.Add(randomLevel);
+            }
+        }
+    }
+
+
+    //nothing needs to happen whenever player collides with it
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            //nothing needs to happen whenever player collides with it
+        }
+    }
+
+}
---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    [SerializeField] private float _speed = 3.0f;
+    [SerializeField] private float _distance;
+
+    private bool movingRight = true;
+
+    public Transform groundDetection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //moves the enemy right
+        transform.Translate(Vector3.right * _speed * Time.deltaTime);
+        //creates a ray starts that shoots downward at the position of ground detection for a distance of 2
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector3.down, _distance);
+        if (groundInfo.collider == false)
+        {
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            } else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        //if other is player
+        //damage player
+        if (other.tag == "Player")
+        {
+            PlayerMovement player = other.transform.GetComponent<PlayerMovement>();
+
+            if (player != null)
+            {
+                player.Damage();
+            }
+        }
+    }
+}
